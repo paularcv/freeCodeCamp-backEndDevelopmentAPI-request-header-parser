ffa0f3c0b290d6405698313d28b58a5a5c8bacce
paula rcv <paula-vetter@web.de>
2025-01-12 16:37:58 +0000

    request header parser

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
+const res = require('express/lib/response');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -22,6 +23,18 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', function(req, res) {
+  let userIpAddress = req.ip;
+  let userLanguage = req.headers['accept-language'];
+  let userSoftware = req.headers['user-agent'];
+  
+  res.json({
+    ipaddress: userIpAddress,
+    language: userLanguage,
+    software: userSoftware
+  });
 });
 
 // listen for requests :)
